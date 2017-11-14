0dad9557b18c50d69f2bd654ba3429086d5215d9
Emil Ekman <tangooe@gmail.com>
2017-11-14 02:38:42 +0100

    Added a BankDataDto

---
+++ ALM_Tenta/Dtos/BankDataDto.cs
@@ -0,0 +1,12 @@
+﻿using ALM_Tenta.Models;
+using System.Collections.Generic;
+
+namespace ALM_Tenta.Dtos
+{
+    public class BankDataDto
+    {
+        public IList<Customer> Customers { get; set; }
+        public IList<Account> Accounts { get; set; }
+        public IList<Transaction> Transactions { get; set; }
+    }
+}
