49dd071455509cc5f1db409430b20fd57e0553a3
Emil Ekman <tangooe@gmail.com>
2017-11-14 02:38:22 +0100

    Added more properties to the Customer Model

--- ALM_Tenta/Models/Customer.cs
+++ ALM_Tenta/Models/Customer.cs
@@ -6,9 +6,14 @@ namespace ALM_Tenta.Models
     {
         public int Id { get; set; }
         public int CustomerNumber { get; set; }
-        public int OrganisationNumber { get; set; }
+        public string OrganisationNumber { get; set; }
         public string Name { get; set; }
-        public string Address { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
         public IList<Account> Accounts { get; set; }
     }
 }
