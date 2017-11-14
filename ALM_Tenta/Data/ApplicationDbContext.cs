f6b92c8be0443a81b187135c75d71446cb6591d9
Emil Ekman <tangooe@gmail.com>
2017-11-14 02:54:52 +0100

    cleaned up the ApplicationDbContext class

--- ALM_Tenta/Data/ApplicationDbContext.cs
+++ ALM_Tenta/Data/ApplicationDbContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ALM_Tenta.Models;
+﻿using ALM_Tenta.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ALM_Tenta.Data
