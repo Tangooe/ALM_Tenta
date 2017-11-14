936588ab987e02bfd76dd8104a29b26c19fa53c0
Emil Ekman <tangooe@gmail.com>
2017-11-14 02:24:48 +0100

    Added Required Models

---
+++ ALM_Tenta/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ALM_Tenta.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public int AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public IList<Transaction> Transactions { get; set; }
+    }
+}
---
+++ ALM_Tenta/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ALM_Tenta.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public int CustomerNumber { get; set; }
+        public int OrganisationNumber { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public IList<Account> Accounts { get; set; }
+    }
+}
