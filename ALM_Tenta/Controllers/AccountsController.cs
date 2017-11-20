2541306b35b893248331ac9df9f1da188352534d
Emil Ekman <tangooe@gmail.com>
2017-11-20 17:48:03 +0100

    Refactored AccountsController

--- ALM_Tenta/Controllers/AccountsController.cs
+++ ALM_Tenta/Controllers/AccountsController.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections.Generic;
-using ALM_Tenta.Data;
+﻿using ALM_Tenta.Data;
 using ALM_Tenta.Models;
-using ALM_Tenta.View_Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
+using ALM_Tenta.ViewModels;
 
 namespace ALM_Tenta.Controllers
 {
@@ -45,30 +44,6 @@ namespace ALM_Tenta.Controllers
             return View(account);
         }
 
-        // GET: Accounts/Create
-        //public IActionResult Create()
-        //{
-        //    ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id");
-        //    return View();
-        //}
-
-        // POST: Accounts/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create([Bind("Id,AccountNumber,Balance,CustomerId")] Account account)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _context.Add(account);
-        //        await _context.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Details), "Customers", new {id = account.CustomerId});
-        //    }
-        //    ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id", account.CustomerId);
-        //    return View(account);
-        //}
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(int customerId)
@@ -82,93 +57,17 @@ namespace ALM_Tenta.Controllers
             return RedirectToAction(nameof(Details), "Customers", new { id = customerId });
         }
 
-        // GET: Accounts/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var account = await _context.Accounts.SingleOrDefaultAsync(m => m.Id == id);
-            if (account == null)
-            {
-                return NotFound();
-            }
-            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id", account.CustomerId);
-            return View(account);
-        }
-
-        // POST: Accounts/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,AccountNumber,Balance,CustomerId")] Account account)
-        {
-            if (id != account.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(account);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AccountExists(account.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id", account.CustomerId);
-            return View(account);
-        }
-
-        // GET: Accounts/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var account = await _context.Accounts
-                .Include(a => a.Customer)
-                .SingleOrDefaultAsync(m => m.Id == id);
-            if (account == null)
-            {
-                return NotFound();
-            }
-
-            if (account.Balance != 0)
-            {
-                TempData["Message"] = "Cannot remove an account with money on it";
-                return RedirectToAction(nameof(Details), "Customers", new { id = account.CustomerId });
-            }
-
-            return View(account);
-        }
-
         // POST: Accounts/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        [HttpPost] 
+        public async Task<IActionResult> Delete(int accountNumber)
         {
-            var account = await _context.Accounts.SingleOrDefaultAsync(m => m.Id == id);
+            var account = await _context.Accounts.SingleOrDefaultAsync(m => m.AccountNumber == accountNumber);
+            var routeId = account.CustomerId;
+
             _context.Accounts.Remove(account);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+
+            return RedirectToAction(nameof(Details), "Customers", new { id = routeId });
         }
 
         [HttpGet]
@@ -253,21 +152,5 @@ namespace ALM_Tenta.Controllers
             TempData["Message"] = $"Failed to withdraw {model.Amount}:- from account {account.AccountNumber}";
             return RedirectToAction(nameof(Details), "Customers", new { id = account.CustomerId });
         }
-
-        private bool AccountExists(int id)
-        {
-            return _context.Accounts.Any(e => e.Id == id);
-        }
-    }
-
-    public class TransferViewModel
-    {
-        public int SenderAccountId { get; set; }
-        public SelectList SenderAccounts { get; set; }
-
-        public int ReciepentAccountId { get; set; }
-        public SelectList ReciepentAccounts { get; set; }
-
-        public decimal Amount { get; set; }
     }
 }
